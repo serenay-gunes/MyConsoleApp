8e9da57ade55757bc1337db000be0dfdd763220d
serenay-gunes <serenaygunes@icloud.com>
2025-01-27 16:07:27 +0300

    u6 tamamlandı

--- u6_dortgenolusturma/Program.cs
+++ u6_dortgenolusturma/Program.cs
@@ -12,8 +12,13 @@ int b = Convert.ToInt32(Console.ReadLine());
 var alan = a * b;
 var cevre = (a+b) * 2; //işlem önceliği için parantez gerekli
 
-Console.WriteLine($"Kenarları {a} ve {b} olan bir dörtgenin alanı {alan}' eder.");
+Console.WriteLine($"Kenarları {a} ve {b} olan \n bir dörtgenin alanı {alan}' eder.");    // ters slash + n ile alt satıra geçilir
 Console.WriteLine($"Kenarları {a} ve {a} olan bir dörtgenin çevresi {cevre}' eder.");
 
 
 
+// dotnet new console --output u7_smt
+// dotnet sln MyConsoleApp.sln add u7_smt/u7_smt.csproj
+// dotnet run --project u6_dortgenolusturma
+
+//burada diğer escape (kaçış) karakterlerinden bahsedildi
