8b57a690554b44962985373cf4788f8bc805df31
serenay-gunes <serenaygunes@icloud.com>
2025-01-27 13:19:42 +0300

    İlk commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Merhaba, .NET konsol uygulaması çalışıyor!");
+
+        // 
+    }
+}
