f2a9382ffbaaf5617258e26a7bc76f89c68f07e5
serenay-gunes <serenaygunes@icloud.com>
2025-01-27 16:46:47 +0300

    u8 eklendi

---
+++ u8_operatorler/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int a = 2, b = 3, c = 6;
+Console.WriteLine($"a = {a}, b = {b}, c = {c}");
+
+a += 3; //a'nın üzerine 3 ekle
+b--; //b'nin değerini 1 azalt
+c =  a++ + b; // a'nın değeri ile b'yi topla, sonucu c'ye aktar, daha sonra a'nın değerini bir arttır (?)
+
+Console.WriteLine($"a = {a}, b = {b}, c = {c}");
