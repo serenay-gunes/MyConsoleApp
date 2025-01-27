d008c009085dcef3bd863ec2ef842c9e51e793f2
serenay-gunes <serenaygunes@icloud.com>
2025-01-27 14:05:00 +0300

    u2_degiskenler klasörü eklendi

---
+++ u2_degiskenler/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+ 
+namespace U2Degiskenler{
+class Program {
+
+    static void Main()
+    {
+        //değişkeni tanımlama ve ilk değer verme
+        int plaka = 35;
+        String sehir = "İzmir";
+        double deger = 125.78;
+        
+        int a, b= 4, c; // gibi de değişken oluşturulabilir, bu satırda değer atamak zorunda değilsin
+
+        a = 1; //kullanılmayınca tanımlandığı yerde altını sarıyla çiziyor
+        c = b; //yapılabilir
+
+        Console.WriteLine(plaka);
+        Console.WriteLine(sehir);
+        Console.WriteLine(deger);
+        Console.WriteLine(a);
+
+    }
+
+
+} 
+
+}
+
+
+
+//DİKKAT:
+// Küçük ı ve büyük İ kullanma, sembol olarak sadece alt tire (_), türkçe karakter kullanma
