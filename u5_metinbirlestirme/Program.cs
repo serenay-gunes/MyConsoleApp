772a4c8ecb401cacddb8883d7ea01870d39be0f7
serenay-gunes <serenaygunes@icloud.com>
2025-01-27 15:39:31 +0300

    u5_eklendi

---
+++ u5_metinbirlestirme/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+int a = 5, b =3;
+int sonuc = a * b;
+
+Console.WriteLine( a + " * " + b + " = " + sonuc); // ilkel yöntem
+
+Console.WriteLine(string.Format("{0} * {1} = {2}",a,b,sonuc)); 
+
+Console.WriteLine("{0} * {1} = {2}",a,b,sonuc); // üstekinin kısası
+
+Console.WriteLine($"{a} * {b} = {sonuc}"); // "string interpolation" -önüne dolar işareti koyuluyor
