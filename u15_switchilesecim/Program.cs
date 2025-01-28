5320958ed00825ba08940734b894c13dc3554ff6
serenay-gunes <serenaygunes@icloud.com>
2025-01-28 13:41:34 +0300

    kosullu ifadeler

---
+++ u15_switchilesecim/Program.cs
@@ -0,0 +1,14 @@
+﻿
+Console.WriteLine("Dönem ortalamasını girin: ");
+int ort = Convert.ToInt32(Console.ReadLine());
+
+string durum = ort switch 
+{
+    >=85 => "Takdir",
+    >=70 => "Teşekkür",
+    >=50 => "Teşekkür",
+    _ => "Sınıfta kaldı."
+    
+};
+
+Console.WriteLine(durum);
