8814992f5162ed3eb2ab15e6fb504a3a1102533d
serenay-gunes <serenaygunes@icloud.com>
2025-01-28 16:08:10 +0300

    donguler devam

---
+++ u21_whiledongusu/Program.cs
@@ -0,0 +1,19 @@
+﻿
+
+String pinKodu = "1234";
+String tahmin = "";
+int deneme = 0;
+
+while(pinKodu != tahmin)
+{
+    if deneme 
+
+    Console.WriteLine("Pin kodunu girin: ");
+    tahmin = Console.ReadLine();
+
+}
+
+Console.WriteLine("Tahmin başarılı.");
+
+
+//BU EKSİK
---
+++ u22_dowhiledongusu/Program.cs
@@ -0,0 +1,15 @@
+﻿//klavyeden 0 girilinceye kadar girilen sayıları toplamak:
+
+ int toplam = 0;
+ int sayi;
+
+        do
+        {
+            Console.WriteLine("Bir sayı girin (çıkış için 0): ");
+            sayi = Convert.ToInt32(Console.ReadLine());
+
+            toplam += sayi; // girilen sayıyı toplama ekle
+
+        } while (sayi != 0); // kullanıcı 0 girene kadar devam et
+
+        Console.WriteLine($"Girilen sayıların toplamı: {toplam}");
