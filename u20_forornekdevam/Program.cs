7a7a7b5e5cef1b71a4f421b768e31124ee64034b
serenay-gunes <serenaygunes@icloud.com>
2025-01-28 15:02:28 +0300

    for dongu ornekleri

---
+++ u20_forornekdevam/Program.cs
@@ -0,0 +1,19 @@
+﻿// Ucgen cizme
+// kullanıcının verdiği yükseklik değerlerine göre üçgen çizmek (* sembolüyle)
+
+
+
+int yuk = 4;
+
+for( int i= 1; i < yuk; i++){
+
+
+    for(int j= 1; j <= i; j++)
+    {
+        Console.Write("*");
+
+    }
+
+    Console.WriteLine();
+    
+}
