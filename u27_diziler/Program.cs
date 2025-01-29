8c67b6726a70b74782885a938ff4189025bfef68
serenay-gunes <serenaygunes@icloud.com>
2025-01-29 11:31:59 +0300

    foreach dongusu ve diziler

--- u27_diziler/Program.cs
+++ u27_diziler/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+int[] sayilar = { 10, 20, 30, 40, 50 };
+
+Console.WriteLine("Sayı adedi: " + sayilar.Length);
+Console.WriteLine("Dizideki sayılar: ");
+
+for (int i = 0; i < sayilar.Length; i++){
+
+    Console.Write($"{sayilar[i]}, ");
+}
+
+// FOR EACH döngüsü:
+
+int toplam = 0;
+
+foreach( int i in sayilar ){ //int olan yere dizinin içindeki veri tipi yazılıyor, ama var da yazılabilir
+
+        toplam += i;
+
+}
+
+Console.WriteLine($"\nDizideki sayıların toplamı: {toplam}");
